595b34bd4a8848c0809635015ea2c9c65da8967e
강배준 <baejun_k@naver.com>
2023-02-18 20:52:41 +0900

    초기 커밋.

---
+++ EnumToComboboxExample/TestEnum.cs
@@ -0,0 +1,16 @@
+﻿namespace EnumToComboboxExample
+{
+	public enum TestEnum
+	{
+		Value01, 
+		Value02, 
+		Value03, 
+		Value04, 
+		Value05, 
+		Value06, 
+		Value07, 
+		Value08, 
+		Value09, 
+		Value10
+	}
+}
---
+++ EnumToComboboxExample/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EnumToComboboxExample
+{
+	public class ViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		public void OnPropertyChanged([CallerMemberName]string? propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+
+		private TestEnum _selectedValue;
+
+		public TestEnum SelectedValue
+		{
+			get { return _selectedValue; }
+			set { _selectedValue = value; OnPropertyChanged(nameof(SelectedValue)); }
+		}
+
+
+		public ViewModel()
+		{
+			_selectedValue = TestEnum.Value01;
+		}
+
+
+
+
+
+
+
+
+	}
+}
